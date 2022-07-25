6ffb080980abfd8e96c0b362e33cc55a7c34627a
codorco <william.emerson@hotmail.com>
2022-07-25 20:31:48 -0300

    do

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-    
+echo "<h1>Teste de variaveis comuns</h1>";
+#      TESTE DE VARIAIS COMUNS      #
+
 $salario = 1500;
 $aumento = 6/100;
 $novosalario = ($salario * $aumento) + $salario;
@@ -10,8 +12,7 @@ echo "novo salario: $novosalario<br>";
 
 
 echo " <br>";
-
-
+echo "<h1>Teste de variaveis 'IF'</h1>";
 #       TESTE DE VARIAVEL "IF"
 
 if (1>0) {
@@ -140,7 +141,10 @@ if ($key1 || $key2 || $key3 || $key4 || $key5) {
     echo "Nenhuma das chaves abriu a porta</br>";
     }
 
-echo " <br>";
+
+
+    echo " <br>";
+    echo "<h1>Teste de variaveis 'FOR'</h1>";
 
 ##      ESTRUTURA DE REPETIÇÃO "FOR"       ##
 
@@ -148,11 +152,15 @@ for ($repete = 0 ;$repete < 10; $repete++) {
     echo "$repete</br>";
 }
 
+echo "<br>";
+
 ##      ESTRUTURA DE REPETIÇÃO "FOR" com paravras   ##
 
 for ($r= 0 ; $r <10 ; $r ++) {
     echo "ola mundo $r</br>";
 }
+
+echo "<br>";
 
 ##      ESTRUTURA DE REPETIÇÃO "FOR" com titulos diminuindo   ##
 
@@ -160,7 +168,8 @@ for ($r= 1 ; $r <= 6 ; $r ++) {
     echo "<h$r>Titulo de nivel $r</h$r></br>";
 }
 
-
+echo " <br>";
+echo "<h1>Teste de variaveis 'FOR'com 'Arraya'</h1>";
 ##      ESTRUTURA DE REPETIÇÃO "FOR" com "Arraya"   ##
 
                     #  0  ,   1  ,   2   ,    3      ,    4      ,      5       ; #
@@ -171,13 +180,14 @@ for ($c=0;$c < 6; $c ++) {
 }
 echo "loop executado com sucesso!<br><br>";
 
+echo "<br>";
 ##      DESIGNANDO OS INDICES  ##
 
                         #            funcionario ,  0    ,   1   ,    2    ,    3   ,    4    ,    5    ,     6     ; #                       
 $funcionarios = array ("funcionario"=>"Gabriel", "Felipe", "Caio", "Marcos", "Bruno", "Flavio", "Albino", "silvio");
     echo $funcionarios ["0"]."</br></br>"; 
 
-
+echo "<br>";
 ##      ESTRUTURA DE "Arraya"  ##
 
                      #     0    ,   1   ,    2    ,    3   ,    4    ,    5    ,     6   ,    7     ; #                       
@@ -188,7 +198,7 @@ $funcionario = array ("Gabriel", "Felipe", "Caio", "Marcos", "Bruno", "Flavio", 
 }
 
 echo "<br>";
-
+echo "<h1>Teste de variaveis 'While'com 'Arraya'</h1>";
 ##      ESTRUTURA DE REPETIÇAO "While"  ##
 
 $coisa = 0;
@@ -223,19 +233,77 @@ do{
     $tentativa ++;
 } while ($tentativa <4);
 
+echo "<br>";
+echo "<h1>Teste de variaveis 'foreach'</h1>";
+
+##      ESTRUTURA DE REPETIÇAO "foreach ($ as $)"   ##
+
+$categorias2 = array ("FPS", "RPG", "Acão", "Aventura", "Simulção", "Plataforma");
+
+foreach ($categorias2 as $valores3) {
+    echo "$valores3<br>";
+
+}
+
+echo "<br>";
+
+##      ESTRUTURA DE REPETIÇAO "foreach ($ as $)" Com numeros de valores   ##
 
 
+$categorias3 = array ("FPS", "RPG", "Acão", "Aventura", "Simulção", "Plataforma");
 
+foreach ($categorias3 as $indice => $valores4) {
+    echo "Posiçao: $indice - [$valores4]<br>";
 
+}
 
+echo "<br>";
 
+##      Ver dentro de uma "Array"   ##
 
+$categorias4 = array ("FPS", "RPG", "Acão", "Aventura", "Simulção", "Plataforma");
 
+var_dump($categorias4); 
 
+echo "<br>";
+echo "<h1>Teste de variaveis 'FOR junto com IF'</h1>";
 
+##      Teste de variaveis 'FOR junto com IF   ##
+// Se eu quiser negar é só colocar "for (!($pp = 0; $pp < 10; $pp++)) {}"
+for ($pp = 0; $pp < 10; $pp++) {
+    if ($pp > 2 && $pp <8) {
+        echo "$pp<br>";
+    }
+}
+echo "<br>";
+##      Teste de variaveis 'FOR junto com IF' Ao contrario   ##
 
+for ($pp = 0; $pp < 10; $pp++) {
+    if (!($pp > 2 && $pp <8)) {
+        echo "$pp<br>";
+    }
+}
+echo "<br>";
+    ##      Teste de variaveis 'FOR junto com IF' Imprimir somente numeros pares   ##
 
+ for ($pp = 0; $pp < 10; $pp++) {
+    if ($pp % 2 == 0) {
+        echo "$pp<br>";
+    }
+}
+echo "<br>";
 
+ ##      Teste de variaveis 'FOR junto com IF' Imprimir pares e impares com letras   ##
+
+ for ($pp = 0; $pp < 10; $pp++) {
+    if ($pp % 2 == 1) {
+        echo "Impar $pp<br>";
+    } else {
+        echo "par $pp<br>";
+
+    }
+}
+echo "<br>";
 
 
 
