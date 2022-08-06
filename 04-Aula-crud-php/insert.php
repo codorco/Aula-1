7dbe639fc1dbb62dba75eb05ad5ea36f56b3e51b
codorco <william.emerson@hotmail.com>
2022-08-05 21:46:13 -0300

    crud

---
+++ 04-Aula-crud-php/insert.php
@@ -0,0 +1,13 @@
+<?php
+include "chavedb.php";
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+mysqli_query($link, "insert into tb_login(nome,email,senha)
+values('$nome','$email','$senha')");
+header('location: form_insert.php');
+
+
+?>
