4cb6815a19cf787e577a7bf382c3a3ab96f00eeb
codorco <william.emerson@hotmail.com>
2022-08-05 21:46:21 -0300

    crud 2

---
+++ 04-Aula-crud-php/form_insert.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Insert em PHP</title>
+    </head>
+    <body>
+        <form action="insert.php" method="post">
+            <input type="text" name="nome" placeholder="Digite o seu nome">
+            <input type="email" name="email" placeholder="Digite o seu email">
+            <input type="password" name="senha" placeholder="Digite a sua senha">
+            <input type="submit" value="cadastrar">
+        </form>
+    </body>
+
+</html>
---
+++ 04-Aula-crud-php/select.php
@@ -0,0 +1,17 @@
+<?php
+include "chavedb.php";
+
+$sql = mysqli_query($link, "select * from tb_login");
+while($vetor= mysqli_fetch_array($sql)){
+    $id = $vetor['id_login'];
+    $nome = $vetor['nome'];
+    $email = $vetor['email'];
+    $senha = $vetor['senha'];
+
+    echo "Id: $id<br>";
+    echo "Nome: $nome<br>";
+    echo "E-mail: $email<br>";
+    echo "Senha: $senha<hr>";
+}
+
+?>
